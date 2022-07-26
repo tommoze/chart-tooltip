6c4be90e4c6c731b3319a69d2e451e505fbb5963
tommoze <33844718+tommoze@users.noreply.github.com>
2022-07-26 23:02:27 +0300

    chart tooltip

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import {
+  VictoryChart,
+  VictoryTheme,
+  VictoryLine,
+  VictoryVoronoiContainer,
+  VictoryTooltip,
+  VictoryLabel,
+} from "victory-native";
 
-export default function App() {
+const Flyout = (props) => {
+  // console.log("props", props);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View
+      style={{
+        position: "absolute",
+        left: props.x,
+        top: 50,
+        height: 250,
+        width: 1,
+        backgroundColor: "red",
+        zIndex: 5,
+      }}
+    ></View>
   );
+};
+
+const data = [
+  { x: 1, y: 2 },
+  { x: 2, y: 3 },
+  { x: 3, y: 5 },
+  { x: 4, y: 4 },
+  { x: 5, y: 7 },
+];
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <VictoryChart
+          theme={VictoryTheme.material}
+          containerComponent={
+            <VictoryVoronoiContainer
+              voronoiDimension="x"
+              labels={({ datum }) => [datum.x, datum.y]}
+              labelComponent={
+                <VictoryTooltip
+                  activateData
+                  center={{ x: 30, y: 20 }}
+                  flyoutComponent={<Flyout />}
+                  labelComponent={
+                    <VictoryLabel
+                      style={[{ fill: "red", fontSize: 25 }]}
+                      backgroundStyle={{ fill: "white" }}
+                    />
+                  }
+                />
+              }
+            />
+          }
+        >
+          <VictoryLabel dx={20} dy={20} text="test" />
+          <VictoryLine
+            style={{
+              data: { stroke: "#c43a31" },
+              parent: { border: "1px solid #ccc" },
+            }}
+            data={data}
+          />
+        </VictoryChart>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5fcff",
   },
 });
